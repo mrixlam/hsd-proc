e530e44e3d8e041b462c01f33fa3ee413160bc59
Rubaiat Islam <mrislam386@gmail.com>
2024-06-20 15:33:40 -0600

    code simplification and troubleshoot for loop

--- proc_with_ioda/convert_hsd_data_with_obs2ioda_serial.sh
+++ proc_with_ioda/convert_hsd_data_with_obs2ioda_serial.sh
@@ -1,57 +1,49 @@
 #!/bin/bash
 
 # Define the base directory path
-base_dir="/glade/derecho/scratch/mrislam/work/pandac/radiance_data_processing/obs_ioda/work"
-
-# Define the ioda directory path
-ioda_dir="$base_dir/output"
-
-# Create the ioda directory if it doesn't exist
-mkdir -p "$ioda_dir"
+input_dir="/glade/derecho/scratch/mrislam/work/pandac/data/himawari"
+output_dir="/glade/derecho/scratch/mrislam/work/pandac/conversion/obs2ioda/himawari"
 
 # Define the list of times
-times=('0000' '0010' '0020' '0030' '0040' '0050' '0100' '0110' '0120' '0130' '0140' '0150' 
-       '0200' '0210' '0220' '0230' '0240' '0250' '0300' '0310' '0320' '0330' '0340' '0350' 
-       '0400' '0410' '0420' '0430' '0440' '0450' '0500' '0510' '0520' '0530' '0540' '0550' 
-       '0600' '0610' '0620' '0630' '0640' '0650' '0700' '0710' '0720' '0730' '0740' '0750' 
-       '0800' '0810' '0820' '0830' '0840' '0850' '0900' '0910' '0920' '0930' '0940' '0950' 
-       '1000' '1010' '1020' '1030' '1040' '1050' '1100' '1110' '1120' '1130' '1140' '1150' 
-       '1200' '1210' '1220' '1230' '1240' '1250' '1300' '1310' '1320' '1330' '1340' '1350' 
-       '1400' '1410' '1420' '1430' '1440' '1450' '1500' '1510' '1520' '1530' '1540' '1550' 
-       '1600' '1610' '1620' '1630' '1640' '1650' '1700' '1710' '1720' '1730' '1740' '1750' 
-       '1800' '1810' '1820' '1830' '1840' '1850' '1900' '1910' '1920' '1930' '1940' '1950' 
-       '2000' '2010' '2020' '2030' '2040' '2050' '2100' '2110' '2120' '2130' '2140' '2150' 
-       '2200' '2210' '2220' '2230' '2240' '2250' '2300' '2310' '2320' '2330' '2340' '2350')
+times=('0000' '0100' '0200' '0300' '0400' '0500' '0600' '0700' '0800' '0900' '1000' '1100'  
+       '1200' '1300' '1400' '1500' '1600' '1700' '1800' '1900' '2000' '2100' '2200' '2300')
 
-# Loop through each directory in the specified base directory
-for dir in "$base_dir"/input/201*; do
-    if [[ -d "$dir" ]]; then  # Check if it's a directory
-        yyyymmdd=$(basename "$dir") # Extract the directory name
-        echo "The extracted date (yyyymmdd) is: $yyyymmdd"
-        
-        # Create a new directory inside the ioda directory with the name of the extracted date
-        new_dir="$ioda_dir/$yyyymmdd"
-        mkdir -p "$new_dir"
-        
-        # Change to the new directory
-        cd "$new_dir"
-        
-        # Create a symbolic link inside the new directory
-        ln -svf /glade/derecho/scratch/mrislam/work/pandac/radiance_data_processing/obs2ioda/obs2ioda-v2/src/obs2ioda-v2.x ./
+# Construct the list of yyyymmdd directories
+dirs=("$input_dir"/2017/2017*)
+num_dirs=${#dirs[@]}
 
-        # Loop through each time in the list
-        for hhmm in "${times[@]}"; do
-            # Define the data file path with the current time
-            data_file="$base_dir/input/$yyyymmdd/HS_H08_${yyyymmdd}_${hhmm}_B01_FLDK_R20_S0101.DAT"
-        
-            # Check if the data file exists
-            if [[ -f "$data_file" ]]; then
-                # Run the obs2ioda-v2.x executable with the data file
-                ./obs2ioda-v2.x -i "$base_dir/input/$yyyymmdd" -ahi -t ${yyyymmdd}${hhmm} -s 72
-            else
-                echo "Data file $data_file not found!"
-            fi
-        done
+# Extract the yyyymmdd part from each directory path and store them in an array
+yyyymmdd_list=()
+for dir in "${dirs[@]}"; do
+    if [[ -d "$dir" ]]; then
+        yyyymmdd=$(basename "$dir")
+        yyyymmdd_list+=("$yyyymmdd")
     fi
+done
+
+# Print the yyyymmdd_list array
+echo "List of yyyymmdd directories: ${yyyymmdd_list[@]}"
+
+# Loop over the yyyymmdd_list and convert hsd data with obs2ioda
+for yyyymmdd in "${yyyymmdd_list[@]}"; do
+    # extract yyyy information from yyyymmdd
+    yyyy=${yyyymmdd:0:4}
+    # prepare output directory where we will save converted files
+    new_dir="$output_dir/$yyyy/$yyyymmdd"
+    mkdir -p "$new_dir"
+    # navigate to the output data directory
+    cd "$new_dir"
+    echo "Created and navigated to directory: $new_dir"
+    # create a symbolic link for obs2ioda executable
+    ln -svf /glade/work/mrislam/pandac/software/obs2ioda/obs2ioda-v2/src/obs2ioda-v2.x ./
+    # loop over hhmm and convert hsd data
+    for hhmm in "${times[@]}"; do
+        data_file="$input_dir/2017/$yyyymmdd/HS_H08_${yyyymmdd}_${hhmm}_B01_FLDK_R20_S0101.DAT"
+        if [[ -f "$data_file" ]]; then
+            ./obs2ioda-v2.x -i "$input_dir/2017/$yyyymmdd" -ahi -t ${yyyymmdd}${hhmm} -s 72
+        else
+            echo "Data file $data_file not found!"
+        fi
+    done
 done
 
